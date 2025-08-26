f6597e195a17712fbadd325e73ddc63a7a0b1d76
Yaroslav Mozheitov <129555075+YriKm@users.noreply.github.com>
2025-08-26 18:47:13 +0100

    26.08.2025

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let discount = 0;
+
+  if (days >= 3 && days <= 7) {
+    discount = 20;
+  }
+
+  if (days >= 7) {
+    discount = 50;
+  }
+
+  return days * 40 - discount;
 }
 
 module.exports = calculateRentalCost;
